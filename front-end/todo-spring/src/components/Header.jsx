35fad9beab510683ecf25f601f660b40dbfbee57
“selam” <Selamkd@users.noreply.github.com>
2024-07-06 11:51:47 +0100

    front-end: add header and install tailwind css

---
+++ front-end/todo-spring/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/App.css';
+const Header =() =>{
+    return(
+     <header>
+        <p className="m-10">I'm a header</p>
+     </header>
+    );
+
+}
+
+export default Header;
