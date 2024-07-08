16b5e49b3097a09794d83ff2fff74d75fc2d2c99
“selam” <Selamkd@users.noreply.github.com>
2024-07-08 14:44:16 +0100

    front-end: add header styling

--- front-end/todo-spring/src/components/Header.jsx
+++ front-end/todo-spring/src/components/Header.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React from 'react';
 import '../styles/App.css';
-const Header =() =>{
-    return(
-     <header>
-        <p className="m-10">I'm a header</p>
-     </header>
-    );
-
-}
+const Header = () => {
+  return (
+    <header className="top-0 fixed border-b-2 border-gray-400 w-full py-2">
+      <p>I'm a header</p>
+    </header>
+  );
+};
 
 export default Header;
